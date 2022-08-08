a606db3d105896ffbd9fb98decfb60ea079c1384
Vadim <fishermanvadik@gmail.com>
2022-08-08 20:57:07 +0300

    Решил задачу 27

--- Program.cs
+++ Program.cs
@@ -20,4 +20,28 @@ int Exponentiation(int numberA, int numberB){
 
 
 
-*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Для определения суммы цифр ведите число : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+  int SumNum(int num){
+    
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++){
+      advance = num - num % 10;
+      result = result + (num - advance);
+      num = num / 10;
+    }
+   return result;
+  }
+
+int sumNum = SumNum(num);
+Console.WriteLine("Сумма цифр : " + sumNum);
+
+
+
