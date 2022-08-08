9faee418586665d07b4f6d4d4a84938fe8349e9f
Vadim <fishermanvadik@gmail.com>
2022-08-08 21:19:13 +0300

    Решил задачу 29

--- Program.cs
+++ Program.cs
@@ -44,4 +44,85 @@ int sumNum = SumNum(num);
 Console.WriteLine("Сумма цифр : " + sumNum);
 
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
+Console.Write("Через запятую введите числа : ");
+string? seriesOfNumbers = Console.ReadLine();
+
+seriesOfNumbers = seriesOfNumbers + ",";    // Запятая для обозначения конца строки
+
+// Метод для удаления пробелов из строки 
+string DeleteSpaces (string series){
+  string seriesNew = "";
+  for (int i = 0; i < series.Length; i++)
+  {
+    if (series[i] != ' ') 
+    {
+      seriesNew += series[i];
+    }
+  }
+  return seriesNew;
+}
+
+//  Метод для проверки на правильность ввода 
+void ErrorCheck (int  series){
+
+      if (series == '0'||series == '1'||series == '2'
+      ||series == '3'||series == '4'||series == '5'||series == '6'
+      ||series == '7'||series == '8'||series == '9'||series == ','
+      ||series == '-')
+      {
+      }
+        else {
+          Console.WriteLine($"Введите корректные значения, а именно цифры!");
+
+      }
+}
+
+// Метод для создания и заполнения массива из строки
+int[] ArrayOfNumbers(string seriesNew){ 
+
+  int[] arrayOfNumbers = new int[1];   
+
+  int j =0;
+
+  for (int i = 0; i < seriesNew.Length; i++){
+    string seriesNew1 = "";
+
+    while (seriesNew[i] != ',' && i < seriesNew.Length){
+      seriesNew1 += seriesNew[i];
+      ErrorCheck(seriesNew[i]);
+      i++;
+    }
+    arrayOfNumbers[j] = Convert.ToInt32(seriesNew1);    
+    if (i < seriesNew.Length-1){
+      arrayOfNumbers = arrayOfNumbers.Concat(new int[] {0}).ToArray();    
+    }
+    j++;
+  }
+  return arrayOfNumbers;
+}
+
+// Метод для вывода массива
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  Console.Write("[");
+  while(index < count){
+    Console.Write(coll[index]);
+    index++;
+    if (index < count){
+      Console.Write(", ");
+    }
+  }
+  Console.Write("]");
+} 
+
+
+string seriesNew = DeleteSpaces(seriesOfNumbers);
+
+int[] arrayOfNumbers =  ArrayOfNumbers(seriesNew);
+
+PrintArry(arrayOfNumbers);
+
+*/
