9af757076fafc2774ec77e1cda0174e16b588454
Fisher606 <108901333+Fisher606@users.noreply.github.com>
2022-08-12 02:01:31 +0300

    Add files via upload

---
+++ HomeWorkNumber5.cs
@@ -0,0 +1,39 @@
+﻿/*
+// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] numbers = new int[6];
+
+void FillArray(int[] array, int min, int max){          //Метод заполнения массива
+  for (int i = 0; i<array.Length; i++ ){
+    array[i] = new Random().Next(min, max);
+  }
+}
+
+void WriteArray(int[] array){                           //Метод написания массива
+    for (int i = 0; i<array.Length; i++ ){
+    Console.Write(array[i] + " ");
+  }
+  Console.WriteLine();
+}
+
+int Quantity(int[] array){                      //Метод определения количества четных чисел
+    int quantity = 0;
+    for (int i = 0; i<array.Length; i++ ){
+    if (array[i] % 2 == 0)
+    {
+      quantity++;
+    }
+  }
+  return quantity;
+}
+
+FillArray(numbers, 100, 1000);
+WriteArray(numbers);
+Console.WriteLine();
+
+int quantity = Quantity(numbers);
+Console.WriteLine($"Четных чисел в массиве {quantity}");
+
+/*
+
+
